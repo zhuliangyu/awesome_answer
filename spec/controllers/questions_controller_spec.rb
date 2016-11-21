4f11cb5b235d2a2605ca66402aeabd42f654cc9a
RickyZhu <riozhu@gmail.com>
2016-11-21 12:45:34 -0800

    add question-controller rspec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -5,12 +5,19 @@ RSpec.describe QuestionsController, type: :controller do
   let(:user) { FactoryGirl.create(:user) }
 
   describe "#create" do
-    it "should insert into database if user sign in " do
+
+    context "user sign in "
+
+    before do
+      session[:user_id]=user.id
+
+    end
+    it "should insert into database" do
 
 
       count_before=Question.count
 
-      session[:user_id]=user.id
+      # session[:user_id]=user.id
 
 
       post(:create, {question: FactoryGirl.attributes_for(:question)})
@@ -24,6 +31,30 @@ RSpec.describe QuestionsController, type: :controller do
 
     end
 
+    it "should redirect to question#show " do
+      # question=FactoryGirl.create(:question,user:user)
+
+      post(:create, {question: FactoryGirl.attributes_for(:question)})
+
+      # expect(response).to(redirect_to question_path(question.id))
+
+
+    end
+
+    it "should have the same user and question creater" do
+      # has problem here!!!
+      post(:create, {question: FactoryGirl.attributes_for(:question)})
+
+
+
+
+    end
+
+
+  end
+
+
+  context "user did not sign in " do
     it "should not insert into database if user does not sign in " do
       count_before=Question.count
 
@@ -37,6 +68,7 @@ RSpec.describe QuestionsController, type: :controller do
 
 
       expect(count_after).to(eq(count_before))
+
 
     end
 
